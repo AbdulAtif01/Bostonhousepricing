ad705f2c69163d2f5ed4322f8529a8c8e579cc1c
AbdulAtif01 <abdulimrann1@gmail.com>
2023-01-07 00:25:38 +0530

    updated

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np 
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+scaler =StandardScaler()
 
 app=Flask(__name__)
 #loading the model
@@ -17,7 +19,7 @@ def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.fit_transform(np.array(list(data.values())).reshape(1,-1))
     output =regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
